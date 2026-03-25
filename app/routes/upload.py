f5677946d8ba373a4e04d58045a19f13eda381ad
Aneesh Desai <desaianeesh2004@gmail.com>
2026-03-25 18:50:17 +0530

    data injestion pipeline is made

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter, UploadFile, File
 import pandas as pd
+import io
 
 router = APIRouter()
 
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     try:
-        # Read file based on type
+        contents = await file.read()   # read file as bytes
+
         if file.filename.endswith(".csv"):
-            df = pd.read_csv(file.file)
+            df = pd.read_csv(io.StringIO(contents.decode("utf-8")))
         elif file.filename.endswith(".xlsx"):
-            df = pd.read_excel(file.file)
+            df = pd.read_excel(io.BytesIO(contents))
         else:
             return {"error": "Unsupported file format"}
 
