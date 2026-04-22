cacde70a176c0edc97db2e7c1c90856742849dec
Aneesh Desai <desaianeesh2004@gmail.com>
2026-04-23 00:32:01 +0530

    Connectivity isssue attended

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.routes.upload import router as upload_router
 from app.routes.analytics import router as analytics_router
+from app.routes.predict import router as predict_router
 
-app = FastAPI()
+app = FastAPI(title="E-WASP Early Warning System")
 
 app.include_router(upload_router)
 app.include_router(analytics_router)
+app.include_router(predict_router)
